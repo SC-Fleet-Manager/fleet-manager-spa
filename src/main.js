8bfce6d0035b6b8847d0c29154a5688966e06941
Mathilde Wattrelos <mat.wattrelos@yahoo.com>
2021-06-03 14:13:14 +0100

    Add vue-gtag

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import App from './App';
 import store from './store/store';
 import VueToastr from "vue-toastr";
 import infiniteScroll from 'vue-infinite-scroll';
+import VueGtag from "vue-gtag";
 import {
     LayoutPlugin,
     FormPlugin,
@@ -86,6 +87,10 @@ Vue.use(SidebarPlugin);
 Vue.use(TabsPlugin);
 Vue.use(FormSelectPlugin);
 Vue.use(infiniteScroll);
+Vue.use(VueGtag, {
+    config: { id: "UA-38751935-8" }
+}, router);
+
 Vue.use(VueToastr, {
     defaultTimeout: 3000,
     defaultProgressBar: false,
